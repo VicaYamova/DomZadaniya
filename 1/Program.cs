6a5cd02d02e004def4aaba07def2df627a1a84e9
Vika <vica_che@mail.ru>
2022-08-19 20:57:25 +0300

    1

---
+++ 1/Program.cs
@@ -0,0 +1,17 @@
+﻿int A = -1;
+int B = 280;
+
+int max = A;
+int min = B;
+
+if(A > max) max = A;
+if(B > max) max = B;
+
+if(A < min) min = A;
+if(B < min) min = B;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+Console.Write("min = ");
+Console.WriteLine(min);
