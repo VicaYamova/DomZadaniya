96e69af4d7ab5aa0416860d557803cc01dd89610
Vika <vica_che@mail.ru>
2022-08-19 21:04:35 +0300

    4

---
+++ 4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine (" Введите число:  ");
+int n = 
+Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+
+while(i<n)
+{
+if(i % 2 == 0 )
+{
+Console.WriteLine(i + " , ");
+} 
+i++;
+}return;
